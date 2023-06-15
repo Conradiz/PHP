7fe0ca9cf28bb5712a9e9db452d925e2ed61464e
Conrado Vinha <conradovinha@gmail.com>
2023-06-14 23:06:57 -0300

    adicionando url para API do banco central

---
+++ desafios/desafio04/gera.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="fomr.css">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <h1>Resultado</h1>
+        <p>
+            <?php 
+                //pegando a data inicio (pegamos a data atual do sistema e subtraimos por 7)
+                $inicio = date("m-d-Y", strtotime("-7 days"));
+                //pegando a data atual do sistema como data final
+                $final = date("m-d-Y");
+                $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''.$inicio.'\'&@dataFinalCotacao=\''.$final.'\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+            ?>
+        </p>
+    </main>
+    
+</body>
+</html>
