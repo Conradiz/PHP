730c8731a5ee32735b5f2901f05a7104b31ea88d
Conrado Vinha <conradovinha@gmail.com>
2023-06-15 14:45:14 -0300

    up index.php

--- desafios/desafio06/index.php
+++ desafios/desafio06/index.php
@@ -8,10 +8,36 @@
 </head>
 <body>
     <?php 
-    
+        $dividendo = $_GET["divid"] ??0;
+        $divisor = $GET["divis"];
     ?>
     <main>
         <h1>Anatomia de uma divisão</h1>
-    </main>    
+        <form action="<?$_SERVER['PHP_SELF']?>" method="get">
+            <label for="">Dividendo</label>
+            <input type="number" name="divis" id="divis" value="<?=$dividendo?>">
+            <label for="divisor">divisor</label>
+            <input type="number" name="divis" id="divis" min="0" value="<?=$divisor ?>">
+            <input type="submit" value="Analisar">
+        </form>
+    </main>   
+    <section>
+        <h2>Estrutura da divisão</h2>
+        <?php
+            //calculos
+            $quociente = intdiv( $dividendo, $divisor);
+            $resto = $dividendo % $divisor;
+        ?>
+        <table class="divisao">
+            <tr>
+                <td><?=$dividendo ?></td>
+                <td><?=$divisor?></td>
+            </tr>
+            <tr>
+                <td><?=$resto?></td>
+                <td><?=$quociente?></td>
+            </tr>
+        </table>
+    </section> 
 </body>
 </html>
