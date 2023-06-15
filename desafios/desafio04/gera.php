e498d56f7899a5fd46f677a2459263ee2f545536
Conrado Vinha <conradovinha@gmail.com>
2023-06-14 23:09:28 -0300

    update

--- desafios/desafio04/gera.php
+++ desafios/desafio04/gera.php
@@ -16,6 +16,12 @@
                 //pegando a data atual do sistema como data final
                 $final = date("m-d-Y");
                 $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''.$inicio.'\'&@dataFinalCotacao=\''.$final.'\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+                //pegando o valor da cotacao de compra do dolar
+                $dados = json_decode(file_get_contents($url), true);
+                $cotacao = $dados["value"][0]["cotacaoCompra"];
+                //pegando o valor do formulario
+                $valor = $_GET["valor"];
+                $dolar = $valor / $cotacao;
             ?>
         </p>
     </main>
