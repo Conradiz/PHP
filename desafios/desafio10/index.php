7cd6978b1efd4aec0ac75c86f802afc50916b11b
Conrado Vinha <conradovinha@gmail.com>
2023-06-16 11:10:18 -0300

    up index

--- desafios/desafio10/index.php
+++ desafios/desafio10/index.php
@@ -1,11 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    
+    <main>
+        <?php 
+            $anonascimento = $_GET["anonas"];
+        ?>
+        <h1>Calculando sua idade</h1>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+            <label for="anonas">Em que ano você nasceu?</label>
+            <input type="number" name="anonas" id="anonas" value="<?=$anonascimento?>">
+        </form>
+    </main>
 </body>
 </html>
