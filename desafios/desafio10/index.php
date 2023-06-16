e5db588633f10894b2e00a10b31fc0c42e3c8971
Conrado Vinha <conradovinha@gmail.com>
2023-06-16 12:34:53 -0300

    alter index.php

--- desafios/desafio10/index.php
+++ desafios/desafio10/index.php
@@ -3,19 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="desaf.css">
     <title>Document</title>
 </head>
 <body>
     <main>
         <?php 
-            $anonascimento = $_GET["anonas"];
+            $anonascimento = $_GET["anonas"]?? 2000;
             $ano = date("Y");
+            $anoatual = $_GET[""]
         ?>
         <h1>Calculando sua idade</h1>
         <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
             <label for="anonas">Em que ano você nasceu?</label>
             <input type="number" name="anonas" id="anonas" value="<?=$anonascimento?>">
-            <label for="">Quer saber sua idade em que ano</label>
+            <label for="">Quer saber sua idade em que ano? (atualmente estamos em <strong> <?=$ano?></strong>)</label>
+            <input type="number" name="anoatual" value="<?=$anoatual?>">
+            <input type="submit" value="Qual será minha idade?">
         </form>
     </main>
     <section>
