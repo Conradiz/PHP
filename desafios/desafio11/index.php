f4865a456f39b02b9268af93f79a07209f8a2aaa
Conrado Vinha <conradovinha@gmail.com>
2023-06-17 13:30:01 -0300

    alterações

--- desafios/desafio11/index.php
+++ desafios/desafio11/index.php
@@ -3,34 +3,39 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="desafio.css">
+    <link rel="stylesheet" href="form.css">
     <title>Document</title>
 </head>
 <body>
     <?php 
-        $preco = $_GET["prec"];
-        $porcentagem = $_GET["por"];
+        $preco = $_GET["pre"]?? 0;
+        $porcentagem = $_GET["rea"]?? 0;
     ?>
     <main>
-        <h1>Reajustadoer de Preços</h1>
+        <h1>Reajustador de Preços</h1>
         <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
-            <label for="">Preço do Produto (R$)</label>
-            <input type="number" name="prec" id="prec" value="">
-            <label for="por">Qual será p percentual de reajuste? (<strong><span id="pos">?</span>%</strong>)</label>
-            <input type="range" name="por" id="por" min="0" max="100">
+            <label for="">Preço do produto (R$)</label>
+            <input type="number" name="pre" id="pre" value="<?=$preco?>" step="0.01">
+            <label for="reaj">Qual será o percentual do reajuste(<strong><span id="por">?</span>%</strong>)</label>
+            <input type="range" name="rea" id="rea" min="0" max="100" value="<?=$porcentagem?>" oninput="mudaValor()">
             <input type="submit" value="Reajustar">
         </form>
     </main>
-    <?php 
-
-    ?>
     <section>
-        <h2>Resultado do reajuste</h2>
-        <p>O produto que custava R$ com % de aumento vai passar a custar R$ a partir de agora</p>
+        <h2>Resultado do Reajuste</h2>
+        <?php 
+            $reajuste = $preco * $porcentagem / 100;
+            $final = $reajuste + $preco;
+
+            echo "<p> O produto que custava R$".number_format($preco, 2, ",",".") ." com $porcentagem% de aumento vai passar a custar R$ $final a partir de agora</p>"
+        ?>
     </section>
     <script>
-        function mudaPor(){
-            pos.innerText = por.value;
+        //Declaraçoes automaticas
+        mudaValor()
+
+        function mudaValor(){
+            por.innerText = rea.value;
         }
     </script>
 </body>
