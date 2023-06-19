7dbfe6b1606eb594fa2c5f3497703e88306a5d21
Conrado Vinha <conradovinha@gmail.com>
2023-06-18 23:11:45 -0300

    adições

--- desafios/desafio12/index.php
+++ desafios/desafio12/index.php
@@ -10,7 +10,13 @@
         <h1>Calculadora de tempo</h1>
         <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
             <label for="segundos">Qual é o total de segundos</label>
+            <input type="number" name="segun" id="segun" value="">
+            <input type="submit" value="Calcular">
         </form>
     </main>
+    <section>
+        <h2>Totalizando tudo</h2>
+        <p>Analisando o valor que voce digitou, [?] segundos  a um total de:</p>
+    </section>
 </body>
 </html>
