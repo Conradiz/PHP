136f6285694d64b00ea620b7aba3c77fa7727fa7
Conrado Vinha <conradovinha@gmail.com>
2023-06-18 23:01:01 -0300

    up

--- desafios/desafio12/index.php
+++ desafios/desafio12/index.php
@@ -6,6 +6,11 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <main>
+        <h1>Calculadora de tempo</h1>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+            <label for="segundos">Qual é o total de segundos</label>
+        </form>
+    </main>
 </body>
 </html>
