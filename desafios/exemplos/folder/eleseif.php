bf4427e104b85aff4d8dabfed4bbbb3143af7375
Conrado Vinha <conradovinha@gmail.com>
2023-06-26 23:21:27 -0300

    finish

--- desafios/exemplos/folder/eleseif.php
+++ desafios/exemplos/folder/eleseif.php
@@ -6,6 +6,29 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php 
+        echo "<h1>Trabalhando com comparação</h1>";
+        $a = 10;
+        $b = 10;
+        $c = 5;
+
+        if ($a == $b){
+            echo "A é igual a B<br>";
+        }
+        elseif($a != $b){
+            echo "A é diferente de B<br>";
+        }
+        else{
+            echo "Erro";
+        }
+
+        if ($a > $b && $b > $c ){
+            echo "A é maior que B e B é maior que C";
+        }
+        elseif ($a = $b || $c < $b){
+            echo"mudadndo";
+        }
+
+    ?>
 </body>
 </html>
