b89a3a976517e5f177a11549cab7ddf3e8b15fea
Conrado Vinha <conradovinha@gmail.com>
2023-06-27 23:26:30 -0300

    finish for

---
+++ exemplos/folder/for.php
@@ -0,0 +1,11 @@
+<?php 
+    //FOR é a estrutura de respetição do PHP que é utilizada para saber a quatidade de repetições
+    // for (inicio; condição; incremento)
+    for($i = 0; $i <= 10; $i++){
+        echo "$i <br>";
+    }
+    echo "<hr>";
+    for($i = 10; $i > 0; $i--){
+        echo "$i <br>";
+    }
+?>
