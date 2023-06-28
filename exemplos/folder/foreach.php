3a4e4cafab8edc625814805d6572d33d17400c13
Conrado Vinha <conradovinha@gmail.com>
2023-06-27 23:28:04 -0300

    finish foreach

---
+++ exemplos/folder/foreach.php
@@ -0,0 +1,8 @@
+<?php 
+    $alunos = ["Caio", "Bruna","Bianca"];
+    //var_dump($alunos);
+   // um laço que repete enquanto tem informação  
+    foreach($alunos as $aluno){
+        echo"Nome: $aluno <br>";
+    }
+?>
