5cc808813b25fbdb540ba263ea4b460c37632033
Conrado Vinha <conradovinha@gmail.com>
2023-07-01 00:06:53 -0300

    up

--- exemplos/sql/select_where.php
+++ exemplos/sql/select_where.php
@@ -9,6 +9,16 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h2>Select com Where</h2>
+    <?php 
+        $query_usuario = "SELECT id, nome, email, created, FROM usuarios WHERE nome LIKE 'c%'"; 
+        $resultado_query = $conn->prepare($query_usuario);
+        $resultado_query->execute();
+
+        while ($linha_usuario = $resultado_query->fetch(PDO::FETCH_ASSOC)){
+            extract($linha_usuario);
+            echo"";
+        }
+    ?>
 </body>
 </html>
